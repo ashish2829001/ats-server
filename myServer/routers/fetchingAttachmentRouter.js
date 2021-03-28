801c809fe184a1672e264ba696688376285ced7b
ASHISH <ashish2829003@gmail.com>
2021-03-28 21:41:21 +0530

    initial

--- myServer/routers/fetchingAttachmentRouter.js
+++ myServer/routers/fetchingAttachmentRouter.js
@@ -17,7 +17,7 @@ router.post('/fetchAttachments',auth, async (req, res) => {
         var imap = new Imap({
             user: req.myuser.gmailId,
             password: req.myuser.gmailPassword,
-            host: 'smtp.gmail.com',
+            host: 'imap.gmail.com',
             port: 993,
             secure: true,
             // connTimeout: 10000,
